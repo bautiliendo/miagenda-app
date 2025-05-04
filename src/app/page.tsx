67713b8ec8f4a101165bda02d2ada80fbdae1b67
bautiliendo <juanbautistaliendo1@gmail.com>
2025-05-04 00:59:14 -0300

    trying to fix prod build vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Navbar from "@/components/NavBar";
+import dynamic from 'next/dynamic';
 import Hero from "@/sections/Hero";
 import Features from "@/sections/Features";
 import Pricing from "@/sections/Pricing";
 import Footer from "@/sections/Footer";
 import FAQ from "@/sections/Faq";
+
+const Navbar = dynamic(() => import('@/components/NavBar'), { ssr: false });
 
 export default async function Home() {
 
