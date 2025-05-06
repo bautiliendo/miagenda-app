31b30de034165fcc83bc5cc23ab1aaf96672e766
bautiliendo <juanbautistaliendo1@gmail.com>
2025-05-06 20:18:37 -0300

    deleted navbar wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,19 @@ import Features from "@/sections/Features";
 import Pricing from "@/sections/Pricing";
 import Footer from "@/sections/Footer";
 import FAQ from "@/sections/Faq";
-import NavbarClientWrapper from '@/components/NavbarClientWrapper';
+import Navbar from '@/components/NavBar';
 
 
 export default async function Home() {
 
   return (
     <>
-      <NavbarClientWrapper />
-      <main>
-        <HeroClientWrapper />
-        <Features />
-        <FAQ />
-        <Pricing />
-        <Footer />
-      </main>
+      <Navbar />
+      <HeroClientWrapper />
+      <Features />
+      <FAQ />
+      <Pricing />
+      <Footer />
     </>
   );
 }
